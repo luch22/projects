f95585d5953dddf1777b0d36ad164b4ae2efdeef
Luciano <luciano.cia3@gmail.com>
2023-04-11 13:26:29 +0200

    ajout exo DOM

---
+++ DOM_exo/exo1/script.js
@@ -0,0 +1,21 @@
+/*
+ */
+
+function getRandom() {
+  return Math.floor(Math.random() * 256);
+}
+
+console.log(document.title);
+console.log(getRandom());
+
+document.title = "Ceci est le titre";
+
+document.body.style.backgroundColor =
+  "rgb(" + getRandom() + "," + getRandom() + "," + getRandom() + ")";
+
+console.log(document.body.style.backgroundColor);
+console.log(document.title);
+
+for (const item of document.body.children) {
+  console.log(item.tagName);
+}
