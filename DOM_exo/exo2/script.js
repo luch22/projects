e910515545e144a58d7de3756d16ed313ab88d5e
Luciano <luciano.cia3@gmail.com>
2023-04-11 14:12:47 +0200

    ajout des exo deux du DOM

--- DOM_exo/exo2/script.js
+++ DOM_exo/exo2/script.js
@@ -1,2 +1,24 @@
 /*
  */
+function getRandom() {
+  return Math.floor(Math.random() * 256);
+}
+
+for (const item of document.querySelectorAll(".important")) {
+  item.setAttribute("title", "This is an important item");
+  console.log(item.attributes.title);
+}
+for (const item of document.querySelectorAll("img")) {
+  if (!item.className.includes("important")) {
+    item.style.display = "none";
+  }
+}
+for (const item of document.querySelectorAll("p")) {
+  if (item.className.length != 0) {
+    console.log(item.className);
+  } else {
+    item.style.color =
+      "rgb(" + getRandom() + "," + getRandom() + "," + getRandom() + ")";
+  }
+  console.log(item.textContent);
+}
